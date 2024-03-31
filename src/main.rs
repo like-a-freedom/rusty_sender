b1d8edb8c2ec3ab4be44abeda1e90135fd45f4d8
expl0it <solovey.anton@gmail.com>
2024-03-31 13:18:58 +0300

    Added more strongly typization

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,35 @@
 use std::env;
 use std::fs::File;
-use std::io::{self, BufRead, BufReader, Write};
+use std::io::{BufRead, BufReader, Write};
 use std::net::{TcpStream, UdpSocket};
 use std::time::{Duration, Instant};
 
-fn read_file_line_by_line(file_path: &str) -> io::Result<Vec<String>> {
-    let file = File::open(file_path)?;
-    let reader = BufReader::new(file);
+pub fn read_file_line_by_line(file_path: &str) -> Result<Vec<String>, std::io::Error> {
+    let file: File = File::open(file_path)?;
+    let reader: BufReader<File> = BufReader::new(file);
 
-    let mut lines = Vec::new();
+    let mut lines: Vec<String> = Vec::new();
     for line_result in reader.lines() {
-        let line = line_result?;
+        let line: String = line_result?;
         lines.push(line);
     }
 
     Ok(lines)
 }
 
-fn print_progress(current: usize, total: usize) {
-    let percentage = (current as f32 / total as f32) * 100.0;
+pub fn print_progress(current: usize, total: usize) {
+    let percentage: f32 = (current as f32 / total as f32) * 100.0;
     print!(
         "\rProgress: [{:50}] {}%",
         "=".repeat((percentage as usize) / 2),
         percentage as usize
     );
-    io::stdout().flush().unwrap();
+    std::io::stdout().flush().unwrap();
 }
 
-fn print_stats(total_events: usize, elapsed: Duration) {
-    let elapsed_secs = elapsed.as_secs_f32();
-    let avg_events_per_sec = if elapsed_secs > 0.0 {
+pub fn print_stats(total_events: usize, elapsed: Duration) {
+    let elapsed_secs: f32 = elapsed.as_secs_f32();
+    let avg_events_per_sec: f32 = if elapsed_secs > 0.0 {
         total_events as f32 / elapsed_secs
     } else {
         0.0
@@ -39,7 +39,7 @@ fn print_stats(total_events: usize, elapsed: Duration) {
     println!("Average events per second: {:.2}", avg_events_per_sec);
 }
 
-fn main() -> io::Result<()> {
+pub fn main() -> Result<(), std::io::Error> {
     let args: Vec<String> = env::args().collect();
 
     // Error handling for command line arguments
@@ -48,19 +48,19 @@ fn main() -> io::Result<()> {
         std::process::exit(1);
     }
 
-    let file_path = &args[1];
-    let hostname = &args[2];
-    let port = &args[3];
-    let protocol = &args[4].to_lowercase();
-    let lines = read_file_line_by_line(file_path)?;
-    let total_lines = lines.len();
-    let mut lines_sent = 0;
-    let start_time = Instant::now();
-    let remote_host = format!("{}:{}", hostname, port);
+    let file_path: &str = &args[1];
+    let hostname: &str = &args[2];
+    let port: &str = &args[3];
+    let protocol: &str = &args[4].to_lowercase();
+    let lines: Vec<String> = read_file_line_by_line(file_path)?;
+    let total_lines: usize = lines.len();
+    let mut lines_sent: usize = 0;
+    let start_time: Instant = Instant::now();
+    let remote_host: String = format!("{}:{}", hostname, port);
 
-    match protocol.as_str() {
+    match protocol {
         "tcp" => {
-            let mut stream = TcpStream::connect(remote_host)?;
+            let mut stream: TcpStream = TcpStream::connect(remote_host)?;
             for line in lines {
                 writeln!(stream, "{}", line)?;
                 lines_sent += 1;
@@ -68,8 +68,8 @@ fn main() -> io::Result<()> {
             }
         }
         "udp" => {
-            let local_address = format!("0.0.0.0:{}", 0);
-            let socket = UdpSocket::bind(&local_address)?;
+            let local_address: String = format!("0.0.0.0:{}", 0);
+            let socket: UdpSocket = UdpSocket::bind(&local_address)?;
             for line in lines {
                 socket.send_to(line.as_bytes(), &remote_host)?;
                 lines_sent += 1;
@@ -82,7 +82,7 @@ fn main() -> io::Result<()> {
         }
     }
 
-    let elapsed_time = start_time.elapsed();
+    let elapsed_time: Duration = start_time.elapsed();
     println!();
     print_stats(total_lines, elapsed_time);
 
